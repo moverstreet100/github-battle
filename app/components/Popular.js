3d5047816b15af6abe6c441c44c9c10f9af4e33c
moverstreet100 <overstreet.morgan@gmail.com>
2020-04-12 22:03:46 -0500

    added nav bar

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,57 +1,77 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { fetchPopularRepos } from "../utils/api";
 
-function LanguagesNav ({selected, onUpdateLanguage}) {
-    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Pyton']
-        
-    return (
-        <ul className='flex-center'>
-           {languages.map((language, index) => (
-               <li key={index}>
-                   <button 
-                        className='btn-clear nav-link'
-                        style={ language === selected ? {color: 'rgb(187, 46, 31)'} : null }
-                        onClick={() => onUpdateLanguage(language)}
-                        //dont do this *****"this.updateLanguage(language)*****"
-                        >
-                        {language}
-                   </button>
-               </li>
-           ))}
-        </ul>
-    )
+function LanguagesNav({ selected, onUpdateLanguage }) {
+  const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Pyton"];
+
+  return (
+    <ul className="flex-center">
+      {languages.map((language, index) => (
+        <li key={index}>
+          <button
+            className="btn-clear nav-link"
+            style={language === selected ? { color: "rgb(187, 46, 31)" } : null}
+            onClick={() => onUpdateLanguage(language)}
+            //dont do this *****"this.updateLanguage(language)*****"
+          >
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 LanguagesNav.propTypes = {
-    selected: PropTypes.string.isRequired,
-    onUpdateLanguage: PropTypes.func.isRequired
-}
+  selected: PropTypes.string.isRequired,
+  onUpdateLanguage: PropTypes.func.isRequired,
+};
 
 export default class Popular extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            selectedLanguage: 'All'
-        }
-        this.updateLanguage = this.updateLanguage.bind(this)
-    }
-    updateLanguage (selectedLanguage) {
-        //dont do this *****"this.state.selectedLanguage = selectedLanguage"*****
+    this.state = {
+      selectedLanguage: "All",
+      repos: null,
+      error: null,
+    };
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+  updateLanguage(selectedLanguage) {
+    //dont do this *****"this.state.selectedLanguage = selectedLanguage"*****
+    this.setState({
+      selectedLanguage,
+      error: null,
+      repos: null,
+    });
+
+    fetchPopularRepos(selectedLanguage)
+      .then((repos) =>
         this.setState({
-            selectedLanguage
+          repos,
+          error: null,
         })
-    }
-    render() {
-        const { selectedLanguage } = this.state
+      )
+      .catch(() => {
+        console.warn("Error Fetching repos: ", error);
 
-        return (
-            <React.Fragment>
-                <LanguagesNav
-                    selected={selectedLanguage}
-                    onUpdateLanguage={this.updateLanguage}
-                />
-            </React.Fragment>
-        )
-    }
+        this.setState({
+          error: `There was an error fetching the repositopries`,
+        });
+      });
+  }
+  render() {
+    const { selectedLanguage } = this.state;
+
+    return (
+      <React.Fragment>
+        <LanguagesNav
+          selected={selectedLanguage}
+          onUpdateLanguage={this.updateLanguage}
+        />
+      </React.Fragment>
+    );
+  }
 }
--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import Popular from './components/Popular'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Popular from "./components/Popular";
 
 // component has
 // state
@@ -9,22 +9,18 @@ import Popular from './components/Popular'
 // UI
 //jsx is html written inside of javascript
 
-
 class App extends React.Component {
-    render() {
-        return (
-        <div className='container'> 
-            <Popular/>
-        </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <Popular />
+      </div>
+    );
+  }
 }
 
 //takes two arguments
 //1 React.element
 //2 where to rend3er the element to
 
-ReactDOM.render(
-    <App/>,
-    document.getElementById('app')
-)
+ReactDOM.render(<App />, document.getElementById("app"));
